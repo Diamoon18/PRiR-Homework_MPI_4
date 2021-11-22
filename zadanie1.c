345070918a8bf1d3f9553b6b10909ea8b0d357ec
Dianamoon18 <72127610+Diamoon18@users.noreply.github.com>
2021-11-22 09:29:05 +0100

    Update zadanie1.c

--- zadanie1.c
+++ zadanie1.c
@@ -17,16 +17,16 @@ MPI_Comm_rank( MPI_COMM_WORLD, &numer_proc);
 if(numer_proc == 0){
     result += 4*(pow(-1, numer_proc)/(2*(numer_proc+1)-1));
     printf("Przybliznie PI w procesie nr-%d wynosi = %f\n", numer_proc, result);
-    MPI_Send(&result, 1, MPI_INT, numer_proc+1, tag, MPI_COMM_WORLD);
+    MPI_Send(&result, 1, MPI_DOUBLE, numer_proc+1, tag, MPI_COMM_WORLD);
 }
 
 if(numer_proc != 0){
-    MPI_Recv(&result, 1, MPI_INT, numer_proc-1, tag, MPI_COMM_WORLD, &status);
+    MPI_Recv(&result, 1, MPI_DOUBLE, numer_proc-1, tag, MPI_COMM_WORLD, &status);
     //printf("Result-> %d", result);
     result += 4*(pow(-1, numer_proc)/(2*(numer_proc+1)-1));
     printf("Przybliznie PI w procesie nr-%d wynosi = %f\n", numer_proc, result);
     if(numer_proc != size_n-1){
-        MPI_Send(&result, 1, MPI_INT, numer_proc+1, tag, MPI_COMM_WORLD);
+        MPI_Send(&result, 1, MPI_DOUBLE, numer_proc+1, tag, MPI_COMM_WORLD);
     }
 }
 
